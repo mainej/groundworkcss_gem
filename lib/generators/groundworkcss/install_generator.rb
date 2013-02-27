5a41926e3321f2dc5e6c3a69c4cfce27fb259c26
SideReel <pantera@sidereel.com>
2013-02-27 15:57:13 -0800

    Update lib/generators/groundworkcss/install_generator.rb

--- lib/generators/groundworkcss/install_generator.rb
+++ lib/generators/groundworkcss/install_generator.rb
@@ -8,11 +8,11 @@ module Groundworkcss
     def add_assets
       if ::Rails.version >= '3.1'
         directory "groundworkcss_sub/lib/assets/css", "app/assets/stylesheets/groundworkcss"
-        directory "groundworkcss_sub/lib/assets/scss", "app/assets/stylesheets/groundworkcss"
+        directory "groundworkcss_sub/lib/assets/scss", "app/assets/stylesheets/groundworkcss/scss"
         directory "groundworkcss_sub/lib/assets/js", "app/assets/javascripts/groundworkcss"
-        directory "groundworkcss_sub/lib/assets/coffee", "app/assets/javascripts/groundworkcss"
+        directory "groundworkcss_sub/lib/assets/coffee", "app/assets/javascripts/groundworkcss/coffee"
         directory "groundworkcss_sub/lib/assets/images", "app/assets/images/groundworkcss"
-        directory "groundworkcss_sub/lib/assets/font", "public"
+        directory "groundworkcss_sub/lib/assets/font", "public/fonts"
         directory "groundworkcss_sub/lib/apple-icons", "public"
       else
         directory "groundworkcss_sub/lib/assets", "vendor/assets/groundworkcss"
