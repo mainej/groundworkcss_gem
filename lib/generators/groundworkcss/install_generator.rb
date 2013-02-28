aa97a091a9569051195040ff1d62c4922e0d360f
Gary Hepting <ghepting@gmail.com>
2013-02-28 08:01:50 -0800

    Update install_generator.rb

--- lib/generators/groundworkcss/install_generator.rb
+++ lib/generators/groundworkcss/install_generator.rb
@@ -10,10 +10,10 @@ module Groundworkcss
         # place source files in assets
         directory "groundworkcss_sub/lib/assets/scss", "app/assets/stylesheets/groundworkcss"
         directory "groundworkcss_sub/lib/assets/coffee", "app/assets/javascripts/groundworkcss"
-        directory "groundworkcss_sub/lib/assets/images", "app/assets/images"
         # place compiled files in public
         directory "groundworkcss_sub/lib/assets/css", "public/groundworkcss/css"
         directory "groundworkcss_sub/lib/assets/js", "public/groundworkcss/js"
+        directory "groundworkcss_sub/lib/assets/images", "public/groundworkcss/images"
         directory "groundworkcss_sub/lib/assets/font", "public/groundworkcss/font"
         directory "groundworkcss_sub/lib/apple-icons", "public/groundworkcss/apple-icons"
       else
