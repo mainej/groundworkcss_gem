eccb486d892d76abf8d4d6d18c1fd75d82f1abd4
Gary Hepting <ghepting@gmail.com>
2013-02-28 08:47:51 -0800

    Update install_generator.rb

--- lib/generators/groundworkcss/install_generator.rb
+++ lib/generators/groundworkcss/install_generator.rb
@@ -14,7 +14,7 @@ module Groundworkcss
         directory "groundworkcss_sub/lib/assets/css", "public/groundworkcss/css"
         directory "groundworkcss_sub/lib/assets/js", "public/groundworkcss/js"
         directory "groundworkcss_sub/lib/assets/images", "public/groundworkcss/images"
-        directory "groundworkcss_sub/lib/assets/font", "public/groundworkcss/font"
+        directory "groundworkcss_sub/lib/assets/font", "public/font"
         directory "groundworkcss_sub/lib/apple-icons", "public/groundworkcss/apple-icons"
       else
         directory "groundworkcss_sub/lib/assets", "vendor/assets/groundworkcss"
